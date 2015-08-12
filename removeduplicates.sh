eec83585f908ee4a9c304911bdbc5d942f6282cc
ercas <ercas@users.noreply.github.com>
2015-08-12 19:30:06 -0400

    added recursive functionality to removeduplicates.sh plus organization and speed improvements

--- removeduplicates.sh
+++ removeduplicates.sh
@@ -5,23 +5,25 @@
 
 cmd=gvfs-trash
 verbose=true
+recursive=false
 
 ######### parse options
 
 function usage() {
     cat <<EOF
-usage: $(basename $0) [-hs] [-c command] [directory]
+usage: $(basename $0) [-hrs] [-c command] [directory]
 if directory is not specified, the current directory will be scanned.
        -h           display this message
        -s           silent mode
        -c command   specify what command to use to remove files (default $cmd)
 EOF
 }
-while getopts ":hc:s" opt; do
+while getopts ":hrsc:" opt; do
     case $opt in
         h) usage; exit 0 ;;
-        c) cmd="$OPTARG" ;;
+        r) recursive=true ;;
         s) verbose=false ;;
+        c) cmd="$OPTARG" ;;
         ?) usage; exit 1 ;;
     esac
 done
@@ -42,31 +44,42 @@ function quit() {
 ########## setup
 
 cmd=$(command -v ${cmd%% *} >/dev/null && echo $cmd || echo rm)
-output "using $cmd"
-cd "$(test -z "$1" && echo . || echo "$1")"
-output "checking for duplicates in $(pwd)"
+removed=0
+trap quit SIGINT SIGTERM
 
 ######### remove duplicates
 
-removed=0
-trap quit SIGINT SIGTERM
-for selected in *; do
-    if [ -f "$selected" ]; then
-        output "checking for duplicates of $selected"
-        for compare in *; do
-            if [ -f "$compare" ]; then
-                if $(cmp -s "$selected" "$compare") && ! [ "$(readlink -f "$selected")" == "$(readlink -f "$compare")" ]; then
-                    output "$compare is a duplicate of $selected, removing"
-                    $cmd "$compare"
-                    removed=$((removed+1))
+output "using $cmd"
+function removeduplicates() {
+    cd "$1"
+    output "changed directory to $(pwd)"
+    for selected in *; do
+        if [ -f "$selected" ]; then
+            output "checking for duplicates of $selected"
+            selected_fullname="$(readlink -f "$selected")"
+            for compare in *; do
+                if [ -f "$compare" ]; then
+                    if $(cmp -s "$selected" "$compare") && ! [ "$selected_fullname" == "$(readlink -f "$compare")" ]; then
+                        output "$compare is a duplicate of $selected, removing"
+                        $cmd "$compare"
+                        removed=$((removed+1))
+                    fi
                 fi
+            done
+        elif [ -d "$selected" ]; then
+            if $recursive; then
+                removeduplicates "$selected"
+                cd ..
+                output "changed directory back to $(pwd)"
+            else
+                output "$selected is a directory, skipping"
             fi
-        done
-    elif [ -d "$selected" ]; then
-        output "$selected is a directory, skipping"
-    else
-        output "$selected no longer exists, skipping"
-    fi
-done
+        else
+            output "$selected no longer exists, skipping"
+        fi
+    done
+}
+
+removeduplicates "$(test -z "$1" && echo . || echo "$1")"
 
 quit
