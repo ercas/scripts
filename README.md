03f196d9bb6af4dd526ccf8d2f1c824ceded1dff
ercas <ercas@users.noreply.github.com>
2015-09-05 17:36:38 -0400

    new weather.sh script

--- README.md
+++ README.md
@@ -21,3 +21,7 @@ wplatestgoes.sh
 ---------------
 a script to fetch the two latest images from the noaa environmental visualization laboratory's daily repository (ftp://ftp.nnvl.noaa.gov/View/GOES/), create transition images between them, and set the wallpaper to one of the images every hour, progressing from the second latest image to the latest image.
 ![demo image](wplatestgoes-demo.gif)
+
+weather.sh
+----------
+get the current weather from http://openweathermap.org
---
+++ weather.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+########## defaults
+
+units=standard
+suffix=K
+cityid=4930956
+
+########## parse options
+
+usage() {
+    cat <<EOF
+usage: weather [-cf] [cityid]
+   -c        use celsius
+   -f        use fahrenheit
+   cityid    the location id to get weather information for
+see http://openweathermap.org/help/city_list.txt for a list of cityids
+EOF
+}
+
+case $1 in
+    -c) units=metric; suffix=℃ ; shift 1 ;;
+    -f) units=imperial; suffix=℉; shift 1 ;;
+    -h) usage; exit 0 ;;
+    *) usage; exit 1 ;;
+esac
+
+########## get weather
+
+cityid=$([ -z $1 ] && echo $cityid || echo $1)
+echo $(wget -qO - "api.openweathermap.org/data/2.5/weather?id=$cityid&units=$units" | \
+     grep -oP "(?<=temp\":).*(?=,\"pressure)")$suffix
