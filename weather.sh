3f0b17c462be7d41c5678b31a8055eb400462430
ercas <ercas@users.noreply.github.com>
2015-09-06 15:57:02 -0400

    switched from gnu grep and date to perl for portability

--- weather.sh
+++ weather.sh
@@ -8,27 +8,46 @@ uspeed="m/s"
 
 cityid=5128581
 daily=false
-json=
 
 ########## functions
 
 # get the value of the specified key from the current json (one day's data only)
 getval() {
-    echo "$json" | grep -oP "(?<=$1\":).*?(?=,\")" | tr -d '"}'
+    #echo "$json" | grep -oP "(?<=$1\":).*?(?=,\")" | tr -d '"}'
+    echo "$json" | perl -ne "print \$1 if m{$1\":(.*?),\"}" | tr -d '"}'
+}
+
+# extract the individual days from the json that the openweathermap daily api
+# returns and print each day's json on an individual line. each line can then be
+# treated like a json from the openweathermap current weather api.
+getdays() {
+    #echo "$1" | grep -oP "\"dt\":.*?(?=,{)"
+    perl -e 'while ($ARGV[0] =~ m/(\"dt\":.*?),\{/g) { print "$1\n"; }' "$1"
+}
+
+# drop-in replacement for gnu date because of differences with bsd date
+perldate() {
+    perl -e 'use POSIX qw(strftime); print strftime "$ARGV[1]", localtime $ARGV[0]' $1 $2
 }
 
 ########## parse options
 
 usage() {
     cat <<EOF
-usage: $(basename "$0") [-dhilm] [cityid]
+usage: $(basename "$0") [-dhgilmp] [cityid]
        -d        daily forecast
        -h        display this message and exit
        -i        use imperial
        -l        list all cityids and exit
        -m        use metric
        cityid    get weather information for this city id (use -l to find yours)
-the columns for the list are cityid, city name, latitude, longitude, country
+
+the columns for -l are cityid, city name, latitude, longitude, and country.
+
+by default, grep will be used to get information from the jsons unless it isn't
+    gnu grep, in which case, perl will be used instead; the script depends on
+    grep -P to work, which bsd grep doesn't have. this decision can be overriden
+    by specifying -g or -p.
 EOF
 }
 
@@ -45,33 +64,37 @@ done
 
 shift $((OPTIND-1))
 
-########## get weather
+########## setup
 
 cityid=$([ -z $1 ] && echo $cityid || echo $1)
+########## get weather
+
 if $daily; then
     json=$(wget -qO - "api.openweathermap.org/data/2.5/forecast/daily?id=$cityid&units=$units")
     echo "Weather for $(getval name)"
-    # iterate over the individual days and print data from each
-    echo $json | grep -oP "\"dt\":.*?(?=,{)" | while read json; do
+    getdays "$json" | while read json; do
+#...on $(date --date=@$(getval dt) +%A): $(getval description)
         cat << EOF
-...on $(date --date=@$(getval dt) +%A):
-High:        $(getval max)$utemp
-Average:     $(getval day)$utemp
-Low:         $(getval min)$utemp
-Humidity:    $(getval humidity)%
-Wind:        $(getval speed)$uspeed @ $(getval deg)°
+...on $(perldate $(getval dt) %A): $(getval description)
+High:         $(getval max)$utempcurl 
+Average:      $(getval day)$utemp
+Low:          $(getval min)$utemp
+Humidity:     $(getval humidity)%
+Wind:         $(getval speed)$uspeed @ $(getval deg)°
 EOF
     done
 else
     json=$(wget -qO - "api.openweathermap.org/data/2.5/weather?id=$cityid&units=$units")
+#Sunrise:      $(date --date=@$(getval sunrise) +%
+#Sunset:       $(date --date=@$(getval sunset) +%r)
     cat << EOF
-Weather for $(getval name)
-High:        $(getval temp_max)$utemp
-Current:     $(getval temp)$utemp
-Low:         $(getval temp_min)$utemp
-Humidity:    $(getval humidity)%
-Wind:        $(getval speed)$uspeed @ $(getval deg)°
-Sunrise:     $(date --date=@$(getval sunrise) +%r)
-Sunset:      $(date --date=@$(getval sunset) +%r)
+Weather for $(getval name): $(getval description)
+High:         $(getval temp_max)$utemp
+Current:      $(getval temp)$utemp
+Low:          $(getval temp_min)$utemp
+Humidity:     $(getval humidity)%
+Wind:         $(getval speed)$uspeed @ $(getval deg)°
+Sunrise:      $(perldate $(getval sunrise) %r)
+Sunset:       $(perldate $(getval sunset) %r)
 EOF
 fi
