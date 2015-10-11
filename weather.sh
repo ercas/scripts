bc2ba8dbec660aa91be8a6df7f3ccf2a155c8680
ercas <ercas@users.noreply.github.com>
2015-10-11 14:43:10 -0400

    changed weather.sh to require openweathermap api key

--- weather.sh
+++ weather.sh
@@ -7,6 +7,7 @@ utemp=K
 uspeed="m/s"
 
 cityid=5128581
+apikey=
 daily=false
 
 ########## functions
@@ -34,10 +35,11 @@ perldate() {
 
 usage() {
     cat <<EOF
-usage: $(basename "$0") [-dhilm] [cityid]
+usage: $(basename "$0") [-dhilm] [-k key] [cityid]
        -d        daily forecast
        -h        display this message and exit
        -i        use imperial
+       -k key    use the specified api key
        -l        list all cityids and exit
        -m        use metric
        cityid    get weather information for this city id (use -l to find yours)
@@ -48,11 +50,12 @@ the columns for -l are cityid, city name, latitude, longitude, and country. to
 EOF
 }
 
-while getopts ":dhilm" opt; do
+while getopts ":dhik:lm" opt; do
     case $opt in
         d) daily=true ;;
         h) usage; exit 0 ;;
         i) units=imperial; utemp=°F; uspeed=mph ;;
+        k) apikey="$OPTARG" ;;
         l) wget -qO - http://openweathermap.org/help/city_list.txt; exit 0 ;;
         m) units=metric; utemp=°C ;;
         ?) usage; exit 1 ;;
@@ -66,8 +69,16 @@ shift $((OPTIND-1))
 cityid=$([ -z $1 ] && echo $cityid || echo $1)
 ########## get weather
 
+if [ -z "$apikey" ]; then
+    echo "No api key supplied; please specify one with -k" >&2
+    exit 1
+fi
 if $daily; then
-    json=$(wget -qO - "api.openweathermap.org/data/2.5/forecast/daily?id=$cityid&units=$units")
+    json=$(wget -qO - "api.openweathermap.org/data/2.5/forecast/daily?id=$cityid&units=$units&APPID=$apikey")
+    if [ -z "$json" ]; then
+        echo "Invalid API key. Please see http://openweathermap.org/faq#error401 for more info." >&2
+        exit 1
+    fi
     echo "Weather for $(getval name)"
     getdays "$json" | while read json; do
 #...on $(date --date=@$(getval dt) +%A): $(getval description)
@@ -81,7 +92,11 @@ Wind:         $(getval speed)$uspeed @ $(getval deg)°
 EOF
     done
 else
-    json=$(wget -qO - "api.openweathermap.org/data/2.5/weather?id=$cityid&units=$units")
+    json=$(wget -qO - "api.openweathermap.org/data/2.5/weather?id=$cityid&units=$units&APPID=$apikey")
+    if [ -z "$json" ]; then
+        echo "Invalid API key. Please see http://openweathermap.org/faq#error401 for more info." >&2
+        exit 1
+    fi
 #Sunrise:      $(date --date=@$(getval sunrise) +%
 #Sunset:       $(date --date=@$(getval sunset) +%r)
     cat << EOF
