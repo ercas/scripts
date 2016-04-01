5277dce75dc5e32b90f4153803bc088d5faa5170
ercas <ercas@users.noreply.github.com>
2016-04-01 18:37:36 -0400

    minor serve.sh changes

--- serve.sh
+++ serve.sh
@@ -8,11 +8,11 @@
 # the same directory as the script
 
 [ -z "$1" ] && echo "error: no directory supplied" && exit
-[ -z "$2" ] && config="$(dirname $(readlink -f $0))/serve.sh.conf" || config="$2"
+[ -z "$2" ] && config="$(dirname $(readlink -f $0))/serve.sh.conf" || config="$(readlink -f "$2")"
 ! [ -f "$config" ] && echo "error: could not find $(dirname $(readlink -f $0))/serve.sh.conf" && exit
 
 docker run -d \
     -p 80:80 \
     -v "$(readlink -f "$1")":/var/www/localhost/htdocs/:ro \
     -v "$config":/etc/lighttpd/lighttpd.conf:ro \
-    ercas/lighttpd-fastcgi-bash
+    ercas/lighttpd-fastcgi
