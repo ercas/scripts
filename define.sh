4a38b59588d22631d814a40404295f4ea7fee400
ercas <edgarrimandocastro@gmail.com>
2016-07-04 21:58:36 -0400

    define.sh automatically use less if terminal isn't tall enough

--- define.sh
+++ define.sh
@@ -4,8 +4,33 @@
 
 dic=~/Documents/gutenberg-29765-webster.txt
 
+function define_word() {
+    # assuming that definitions are not longer than 100 lines
+    grep -A 100 "^${1^^}\s*$" $dic | while read line; do
+
+        # if this line is all caps
+        if [ "${line^^}" = "$line" ] && grep -qE "[[:alnum:]]" <<< "$line"; then
+
+            # if this line is the queried word, underline it
+            if [ "$(tr -dc "[:alnum:]" <<< "$line")" = "${1^^}" ]; then
+                echo -e "\e[4m$line\e[m\n"
+
+            # if it's not, then it's the next word in the dictionary
+            else
+                break
+            fi
+
+        # write all other lines normally
+        else
+            echo "$line"
+        fi
+
+    done
+}
+
 [ -z "$1" ] && exit 1
 
+# download a copy of the dictionary if necessary
 if ! [ -f $dic ]; then
     echo -n "couldn't find the dictionary at the set path. download? (Y/n) "
     read ans
@@ -16,25 +41,10 @@ if ! [ -f $dic ]; then
     fi
 fi
 
-# assuming that definitions are not longer than 100 lines
-grep -A 100 "^${1^^}\s*$" $dic | while read line; do
-
-    # if this line is all caps
-    if [ "${line^^}" = "$line" ] && grep -qE "[[:alnum:]]" <<< "$line"; then
-
-        # if this line is the queried word, underline it
-        if [ "$(tr -dc "[:alnum:]" <<< "$line")" = "${1^^}" ]; then
-            echo -e "\e[4m$line\e[m\n"
-
-        # if it's not, then it's the next word in the dictionary
-        else
-            break
-        fi
-
-    # write all other lines normally
-    else
-        echo "$line"
-    fi
-
-done
-
+# open in less if the definition's height is greater than the terminal's
+defn=$(define_word "$1")
+if [ $(wc -l <<< "$defn") -gt $(tput lines) ]; then
+    less -r <<< "$defn"
+else
+    echo "$defn"
+fi
