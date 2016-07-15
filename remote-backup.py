2b9f942cc0a8411e7a9d98a13dc4734ec6e05d45
ercas <edgarrimandocastro@gmail.com>
2016-07-14 23:46:34 -0400

    added extra options list to remote-backup.py and a 5 second grace period

--- remote-backup.py
+++ remote-backup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# frontend for running rsync backup commands
+# frontend for running rsync remote backup commands
 
-import argparse, os, shlex, subprocess, sys
+import argparse, os, shlex, subprocess, sys, time
 
 # default variables to be edited before running this script
 user_local = os.getlogin()
@@ -11,23 +11,9 @@ targets = {
     "template": { # name of the target
         "source": "/path/to/source/directory", # source directory
         "destination": "/path/to/remote/directory/", # destination directory
-        "excludes": [
-            None # directories to exclude
-        ]
-    },
-
-    # sample target
-    "home": {
-        "source": "/home/" + user_local + "/",
-        "destination": "/mnt/backup_disk/" + user_local + "/",
-        "excludes": [
-            ".cache/*",
-            ".local/share/Trash/*",
-            ".local/share/gvfs-metadata/*",
-            ".gvfs/*",
-            ".thumbnails/*",
-        ]
-    },
+        "excludes": [], # directories to exclude
+        "extra-args": [] # default: -vaAE --delete --delete-excluded --ignore-errors --progress
+    }
 
 }
 
@@ -56,12 +42,16 @@ def main():
         mkdir_base_cmd = "torsocks " + mkdir_base_cmd
 
     # create the remote directory if it doesn't exist yet and back up to it
-    subprocess.call(shlex.split(mkdir_base_cmd))
-    subprocess.call(
-            shlex.split(rsync_base_cmd)
-            + ["--exclude=" + exclude for exclude in target["excludes"]]
-            + [target["source"], user_remote + "@" + args.hostname + ":" + target["destination"]]
-    )
+    mkdir_cmd = shlex.split(mkdir_base_cmd)
+    rsync_cmd = shlex.split(rsync_base_cmd) \
+        + [extra_arg for extra_arg in target["extra-args"]] \
+        + ["--exclude=" + exclude for exclude in target["excludes"]] \
+        + [target["source"], user_remote + "@" + args.hostname + ":" + target["destination"]]
+    print(" ".join(mkdir_cmd))
+    subprocess.call(mkdir_cmd)
+    print("running this command in 5 seconds:\n" + " ".join(rsync_cmd))
+    time.sleep(5)
+    subprocess.call(rsync_cmd)
 
 if __name__ == "__main__":
     if user_remote is None:
