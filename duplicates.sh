17e7d6b768e0d4afdc0fe99f37bc92985f19bf0b
ercas <edgarrimandocastro@gmail.com>
2016-07-16 17:23:02 -0400

    can now specify a directory for duplicates.sh

--- duplicates.sh
+++ duplicates.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/sh
 # find duplicate files by comparing md5 checksum
 
-remove_cmd=gvfs-trash
-remove=false # remove files; otherwise, only print duplicates
-keep_first=true # delete all but the first file; otherwise, delete all but the last
+keep_first=true
 find_args="-maxdepth 1"
+remove=false
+remove_cmd=gvfs-trash
+searchdir=$(pwd)
 verbose=false
 
 ########## parse options
@@ -13,15 +14,16 @@ this=$(basename "$0")
 
 function usage() {
     cat <<EOF
-usage: $this [-dhflrv] -c cmd
-       -c cmd    use this command to remove files in delete mode (default is
-                   gvfs-trash; defaults to rm if the command is unavailable)
-       -d        delete mode
-       -h        view this message and exit
-       -f        in delete mode, delete all but the first file (default)
-       -l        in delete mode, delete all but the last file
-       -r        recursive
-       -v        verbose mode
+usage: $this [-dhflrv] [-c cmd] [searchdir]
+       -c cmd       use this command to remove files in delete mode (default is
+                      gvfs-trash; defaults to rm if the command is unavailable)
+       -d           delete mode
+       -h           view this message and exit
+       -f           in delete mode, delete all but the first file (default)
+       -l           in delete mode, delete all but the last file
+       -r           recursive
+       -v           verbose mode
+       searchdir    the directory to search in (default is current directory)
 
 by default, $this will only print all repeated checksums and the files that
 share them; nothing else is done.
@@ -45,6 +47,17 @@ shift $((OPTIND-1))
 
 ########## main
 
+# check if searchdir exists
+if ! [ -z "$1" ]; then
+    if [ -d "$1" ]; then
+        searchdir="$1"
+    else
+        echo "error: \"$1\" is not a directory."
+        exit 1
+    fi
+fi
+
+# check if remove_cmd is available if in remove mode
 base_cmd=$(echo "$remove_cmd" | cut -d " " -f 1)
 if ! command -v $base_cmd > /dev/null && $remove; then
     echo "error: $base_cmd is not available; using rm"
@@ -53,7 +66,8 @@ fi
 
 # calculate checksums
 $verbose && echo "finding files and calculating md5 checksums..."
-sums=$(find . $find_args -type f | sort | tr "\n" "\0" | xargs -0 md5sum | \
+sums=$(find "$searchdir" $find_args -type f | \
+    sort | tr "\n" "\0" | xargs -0 md5sum | \
     tee $($verbose && echo /dev/fd/2 || echo /dev/null))
 
 # find duplicates
