73d6192f217f47b8e431e70506ad252f69108702
ercas <edgarrimandocastro@gmail.com>
2017-12-30 17:20:54 -0500

    rate limiting for all requests

--- scrapers/lunarbaboon.py
+++ scrapers/lunarbaboon.py
@@ -11,57 +11,64 @@ PAGE_URL_BASE = "http://www.lunarbaboon.com/comics/?currentPage="
 
 OUTPUT_DIR = "lunarbaboon"
 
-MAX_ATTEMPTS = 5
+MAX_ATTEMPTS = 10
 
 PAGE_SLEEP = 1
 TOO_MANY_REQUESTS_SLEEP = 5
 IMAGE_SLEEP = 0.5
 
-def scrape_page(page_number, attempt = 1):
-    print("processing page %d (attempt %d)" % (page_number, attempt))
+def retrieve_rate_limited(url, attempt = 1):
+    print("> retrieving %s, attempt %d" % (url, attempt))
 
-    response = requests.get("%s%d" % (PAGE_URL_BASE, page_number))
+    response = requests.get(url)
+
+    if (response.status_code == 200):
+        time.sleep(PAGE_SLEEP)
+        return response
+    elif (response.status_code == 429):
+        print(">> max attempts reached")
+        time.sleep(TOO_MANY_REQUESTS_SLEEP)
+    else:
+        print(">> error %d" % response.status_code)
+        time.sleep(PAGE_SLEEP)
+
+    if (attempt < MAX_ATTEMPTS):
+        retrieve_rate_limited(url, attempt + 1)
+
+def scrape_page(page_number):
+    print("processing page %d" % page_number)
+
+    response = retrieve_rate_limited("%s%d" % (PAGE_URL_BASE, page_number))
     soup = bs4.BeautifulSoup(response.content, "lxml")
 
     posts = soup.findAll("div", {"class": "journal-entry-wrapper"})
 
-    if (len(posts) > 0):
-        for post in posts:
-            title = post.find("h2", {"class": "title"}).text.strip().replace("?", "_").replace("/", "_")
-            date_string = dateutil.parser.parse(
-                post.find("span", {"class": "posted-on"}).text.strip()
-            ).strftime("%Y-%m-%d")[:10]
-            image_url = (
-                "%s/%s" % (
-                    ROOT.rstrip("/"),
-                    post.find("div", {"class": "body"}).find("img")["src"].lstrip("/")
-                )
-            ).split("?")[0]
-            file_ext = image_url.split(".")[-1]
+    for post in posts:
+        title = post.find("h2", {"class": "title"}).text.strip().replace("?", "_").replace("/", "_")
+        date_string = dateutil.parser.parse(
+            post.find("span", {"class": "posted-on"}).text.strip()
+        ).strftime("%Y-%m-%d")[:10]
+        image_url = (
+            "%s/%s" % (
+                ROOT.rstrip("/"),
+                post.find("div", {"class": "body"}).find("img")["src"].lstrip("/")
+            )
+        ).split("?")[0]
+        file_ext = image_url.split(".")[-1]
 
-            output_path = "%s/%s - %s.%s" % (OUTPUT_DIR, date_string, title, file_ext)
+        output_path = "%s/%s - %s.%s" % (OUTPUT_DIR, date_string, title, file_ext)
 
-            if (os.path.isfile(output_path)):
-                print("> %s exists" % output_path)
-            else:
-                image_response = requests.get(image_url)
-                with open(output_path, "wb") as f:
-                    f.write(image_response.content)
-                print("> %s -> %s" % (image_url, output_path))
-                time.sleep(IMAGE_SLEEP)
-
-        time.sleep(PAGE_SLEEP)
-    elif (attempt < MAX_ATTEMPTS):
-        if (response.status_code == 429):
-            print("> error 429 too many requests")
-            time.sleep(TOO_MANY_REQUESTS_SLEEP)
+        if (os.path.isfile(output_path)):
+            print(">> %s exists" % output_path)
         else:
-            print("> error %d" % response.status_code)
-            time.sleep(PAGE_SLEEP)
-        scrape_page(page_number, attempt + 1)
+            image_response = retrieve_rate_limited(image_url)
+            with open(output_path, "wb") as f:
+                f.write(image_response.content)
+            print(">> %s -> %s" % (image_url, output_path))
+            time.sleep(IMAGE_SLEEP)
 
 if (__name__ == "__main__"):
     if (not os.path.isdir(OUTPUT_DIR)):
         os.mkdir(OUTPUT_DIR)
-    for i in range(32, 146):
+    for i in range(1, 146):
         scrape_page(i)
